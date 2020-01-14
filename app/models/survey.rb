29ff4e2f9c572531d21711cff47b8834e96acd4f
Student <example@example.com>
2020-01-14 11:05:27 -0800

    add error messages

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,3 +1,11 @@
 class Survey < ApplicationRecord
   has_many :questions, dependent: :destroy
+   validates :name, presence: true
+   validates_length_of :name, maximum: 100
+   before_save(:titleize_survey)
+
+private
+  def titleize_survey
+    self.name = self.name.titleize
+  end
 end
--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 describe Survey do
   it { should have_many(:questions) }
+  it { should validate_presence_of :name }
+  it { should validate_length_of(:name).is_at_most(100) }
+  it("titleizes the name of an survey") do
+    survey = Survey.create({name: "giant steps"})
+    expect(survey.name()).to(eq("Giant Steps"))
+  end
+
 end
